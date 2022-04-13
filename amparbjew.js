711cf3e2186b0aaefe3b43f62915145cb88a179c
tmo286 <54511022+tmo286@users.noreply.github.com>
2022-04-13 11:46:37 +0300

    Update amparbjew.js

--- amparbjew.js
+++ amparbjew.js
@@ -14,14 +14,7 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/amp/qualt
 					{image : 'b02.jpg'}, 
 					{image : 'b03.jpg'}, 
 					{image : 'b04.jpg'}, 
-					{image : 'b05.jpg'}, 
-					{image : 'b06.jpg'}, 
-					{image : 'b07.jpg'}, 
-					{image : 'b08.jpg'}, 
-					{image : 'b09.jpg'}, 
-					{image : 'b10.jpg'}, 
-					{image : 'b11.jpg'}, 
-					{image : 'b12.jpg'}]
+					{image : 'b05.jpg'}]
 
 			}, 
 			{
@@ -34,13 +27,7 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/amp/qualt
 					{image : 'w03.jpg'}, 
 					{image : 'w04.jpg'}, 
 					{image : 'w05.jpg'}, 
-					{image : 'w06.jpg'}, 
-					{image : 'w07.jpg'}, 
-					{image : 'w08.jpg'}, 
-					{image : 'w09.jpg'}, 
-					{image : 'w10.jpg'}, 
-					{image : 'w11.jpg'}, 
-					{image : 'w12.jpg'}]
+					{image : 'w06.jpg'}]
 			}
 		],
 
