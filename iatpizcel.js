ba61c9c5f736d0e3a11312ee1e2a77a5113e499b
tmo286 <54511022+tmo286@users.noreply.github.com>
2022-08-01 11:28:14 +0300

    Add files via upload

---
+++ iatpizcel.js
@@ -0,0 +1,76 @@
+define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualtrics/quiat9.js'], function(APIConstructor, iatExtension){
+    var API = new APIConstructor();
+
+	return iatExtension({
+		category1 : {
+			name : 'Pizza', //Will appear in the data.
+			title : {
+				media : {word : 'Pizza'}, //Name of the category presented in the task.
+				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
+				height : 4 //Used to position the "Or" in the combined block.
+			}, 
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+    		    {image : 'piz1.jpg'}, 
+    			{image : 'piz2.jpg'}, 
+    			{image : 'piz3.jpg'}, 
+    			{image : 'piz4.jpg'}
+			], 
+			//Stimulus css (style)
+			stimulusCss : {color:'#31940F','font-size':'1.8em'}
+		},	
+		category2 :	{
+			name : 'Celery', //Will appear in the data.
+			title : {
+				media : {word : 'Celery'}, //Name of the category presented in the task.
+				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
+				height : 4 //Used to position the "Or" in the combined block.
+			}, 
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+    		    {image : 'cel1.jpg'}, 
+    			{image : 'cel2.jpg'}, 
+    			{image : 'cel3.jpg'}, 
+    			{image : 'cel4.jpg'}		
+			], 
+			//Stimulus css
+			stimulusCss : {color:'#31940F','font-size':'1.8em'}
+		},
+			attribute1 :
+			{
+				name : 'Good',
+				title : {
+					media : {word : 'Good'},
+					css : {color:'#0000FF','font-size':'1.8em'},
+					height : 4 //Used to position the "Or" in the combined block.
+				},
+				stimulusMedia : [ //Stimuli content as PIP's media objects
+					{word: 'Good'},
+					{word: 'Positive'},
+					{word: 'Pleasant'},
+					{word: 'Great'}
+				],
+				//Stimulus css
+				stimulusCss : {color:'#0000FF','font-size':'2.3em'}
+			},
+			attribute2 :
+			{
+				name : 'Bad',
+				title : {
+					media : {word : 'Bad'},
+					css : {color:'#0000FF','font-size':'1.8em'},
+					height : 4 //Used to position the "Or" in the combined block.
+				},
+				stimulusMedia : [ //Stimuli content as PIP's media objects
+					{word: 'Bad'},
+					{word: 'Negative'},
+					{word: 'Unpleasant'},
+					{word: 'Awful'}
+				],
+				//Stimulus css
+				stimulusCss : {color:'#0000FF','font-size':'2.3em'}
+			},	
+            randomBlockOrder : false,			
+			base_url : {//Where are your images at?
+				image : 'https://tmo286.github.io/scripts-for-Qualtrics/images/'
+			} 			
+	});
+});
