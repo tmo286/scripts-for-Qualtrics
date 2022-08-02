93c83266a23be13a81acd1f037cf34f19c578810
tmo286 <54511022+tmo286@users.noreply.github.com>
2022-08-02 08:46:00 +0300

    Update iatambik.js

--- iatambik.js
+++ iatambik.js
@@ -67,8 +67,7 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualt
 				],
 				//Stimulus css
 				stimulusCss : {color:'#0000FF','font-size':'2.3em'}
-			},	
-            randomBlockOrder : false,			
+			},		
 			base_url : {//Where are your images at?
 				image : 'https://tmo286.github.io/scripts-for-Qualtrics/images/'
 			} 			
