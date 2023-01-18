1abb44961a5ea2c18d003c674203f78ccd0a3e67
tmo286 <54511022+tmo286@users.noreply.github.com>
2023-01-18 16:31:57 +0200

    Update iat_set1_red.js

--- iat_set1_red.js
+++ iat_set1_red.js
@@ -43,7 +43,7 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualt
 				name : 'Good',
 				title : {
 					media : {word : 'Good'},
-					css : {color:'#0000FF','font-size':'1.8em'},
+					css : {color:'#048832','font-size':'1.8em'},
 					height : 4 //Used to position the "Or" in the combined block.
 				},
 				stimulusMedia : [ //Stimuli content as PIP's media objects
@@ -53,14 +53,14 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualt
 					{word: 'Great'}
 				],
 				//Stimulus css
-				stimulusCss : {color:'#0000FF','font-size':'2.3em'}
+				stimulusCss : {color:'#048832','font-size':'2.3em'}
 			},
 			attribute2 :
 			{
 				name : 'Bad',
 				title : {
 					media : {word : 'Bad'},
-					css : {color:'#0000FF','font-size':'1.8em'},
+					css : {color:'#048832','font-size':'1.8em'},
 					height : 4 //Used to position the "Or" in the combined block.
 				},
 				stimulusMedia : [ //Stimuli content as PIP's media objects
@@ -70,8 +70,54 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualt
 					{word: 'Awful'}
 				],
 				//Stimulus css
-				stimulusCss : {color:'#0000FF','font-size':'2.3em'}
+				stimulusCss : {color:'#048832','font-size':'2.3em'}
 			},	
+			instAttributePractice: '<div><p align="center" style="font-size:20px; font-family:arial">' +
+				'<font color="#000000"><u>Part blockNum of nBlocks </u><br/><br/></p>' +
+				'<p style="font-size:20px; text-align:left; vertical-align:bottom; margin-left:10px; font-family:arial">' +
+				'Put a left finger on the <b>E</b> key for items that belong to the category <font color="#048832">leftAttribute.</font>' +
+				'<br/>Put a right finger on the <b>I</b> key for items that belong to the category <font color="#048832">rightAttribute</font>.<br/><br/>' +
+				'If you make a mistake, a red <font color="#ff0000"><b>X</b></font> will appear. ' +
+				'Press the other key to continue.<br/>' +
+				'<u>Go as fast as you can</u> while being accurate.<br/><br/></p>'+
+				'<p align="center">Press the <b>space bar</b> when you are ready to start.</font></p></div>',			
+			instCategoriesPractice: '<div><p align="center" style="font-size:20px; font-family:arial">' +
+				'<font color="#000000"><u>Part blockNum of nBlocks </u><br/><br/></p>' +
+				'<p style="font-size:20px; text-align:left; vertical-align:bottom; margin-left:10px; font-family:arial">' +
+				'Put a left finger on the <b>E</b> key for items that belong to the category <font color="#0C0C0C">leftCategory</font>. ' +
+				'<br/>Put a right finger on the <b>I</b> key for items that belong to the category <font color="#0C0C0C">rightCategory</font>.<br/>' +
+				'Items will appear one at a time.<br/><br/>' +
+				'If you make a mistake, a red <font color="#ff0000"><b>X</b></font> will appear. ' +
+				'Press the other key to continue.<br/>' +
+				'<u>Go as fast as you can</u> while being accurate.<br/><br/></p>'+
+				'<p align="center">Press the <b>space bar</b> when you are ready to start.</font></p></div>',	
+			instFirstCombined : '<div><p align="center" style="font-size:20px; font-family:arial">' +
+				'<font color="#000000"><u>Part blockNum of nBlocks </u><br/><br/></p>' +
+				'<p style="font-size:20px; text-align:left; vertical-align:bottom; margin-left:10px; font-family:arial">' +
+				'Use the <b>E</b> key for <font color="#0C0C0C">leftCategory</font> and for <font color="#048832">leftAttribute</font>.<br/>' +
+				'Use the <b>I</b> key for <font color="#0C0C0C">rightCategory</font> and for  <font color="#048832">rightAttribute</font>.<br/>' +
+				'Each item belongs to only one category.<br/><br/>' +
+				'If you make a mistake, a red <font color="#ff0000"><b>X</b></font> will appear. ' +
+				'Press the other key to continue.<br/>' + 
+				'<u>Go as fast as you can</u> while being accurate.<br/><br/></p>' +
+				'<p align="center">Press the <b>space bar</b> when you are ready to start.</font></p></div>',
+			instSecondCombined : '<div><p align="center" style="font-size:20px; font-family:arial">' +
+				'<font color="#000000"><u>Part blockNum of nBlocks </u><br/><br/></p>' +
+				'<p style="font-size:20px; text-align:left; vertical-align:bottom; margin-left:10px; font-family:arial">' +
+				'This is the same as the previous part.<br/>' +
+				'Use the <b>E</b> key for <font color="#0C0C0C">leftCategory</font> and for <font color="#048832">leftAttribute</font>.<br/>' +
+				'Use the <b>I</b> key for <font color="#0C0C0C">rightCategory</font> and for  <font color="#048832">rightAttribute</font>.<br/>' +
+				'Each item belongs to only one category.<br/><br/>' +
+				'<u>Go as fast as you can</u> while being accurate.<br/><br/></p>' +
+				'<p align="center">Press the <b>space bar</b> when you are ready to start.</font></p></div>',
+			instSwitchCategories : '<div><p align="center" style="font-size:20px; font-family:arial">' +
+				'<font color="#000000"><u>Part blockNum of nBlocks </u><br/><br/></p>' +
+				'<p style="font-size:20px; text-align:left; vertical-align:bottom; margin-left:10px; font-family:arial">' +
+				'<b>Watch out, the labels have changed position!</b><br/>' +
+				'Put the left finger on the <b>E</b> key for <font color="#0C0C0C">leftCategory</font>.<br/>' +
+				'Put the right finger on the <b>I</b> key for <font color="#0C0C0C">rightCategory</font>.<br/><br/>' +
+				'<u>Go as fast as you can</u> while being accurate.<br/><br/></p>' +
+				'<p align="center">Press the <b>space bar</b> when you are ready to start.</font></p></div>',				
             randomBlockOrder : false,			
 			base_url : {//Where are your images at?
 				image : 'https://tmo286.github.io/scripts-for-Qualtrics/images/'
