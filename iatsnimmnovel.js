50e446f67929800d948eca5a174e93ff4011087f
tmo286 <54511022+tmo286@users.noreply.github.com>
2024-04-03 12:12:44 +0300

    Update iatsnimmnovel.js

--- iatsnimmnovel.js
+++ iatsnimmnovel.js
@@ -18,7 +18,7 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualt
 		category2 :	{
 			name : 'BD&B', //Will appear in the data.
 			title : {
-				media : {word : "BD&B"}, //Name of the category presented in the task.
+				media : {word : "Bd&B"}, //Name of the category presented in the task.
 				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
 				height : 4 //Used to position the "Or" in the combined block.
 			}, 
