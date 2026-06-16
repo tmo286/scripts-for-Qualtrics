417f5da4529d75d98b5cb672b588f6f5e67dea2f
tmo286 <54511022+tmo286@users.noreply.github.com>
2026-06-16 10:34:11 +0300

    Add files via upload

---
+++ iatrace.js
@@ -0,0 +1,84 @@
+define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualtrics/quiat9.js'], function(APIConstructor, iatExtension){
+    var API = new APIConstructor();
+
+	return iatExtension({
+		category1 : {
+			name : 'White people', //Will appear in the data.
+			title : {
+				media : {word : 'White people'}, //Name of the category presented in the task.
+				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
+				height : 4 //Used to position the "Or" in the combined block.
+			}, 
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+                {image: 'wm1_nc.jpg'},
+                {image: 'wm2_nc.jpg'},
+                {image: 'wm3_nc.jpg'},
+				{image: 'wf1_nc.jpg'},
+            	{image: 'wf2_nc.jpg'},
+            	{image: 'wf3_nc.jpg'}		
+			], 
+			//Stimulus css (style)
+			stimulusCss : {color:'#31940F','font-size':'1.8em'}
+		},	
+		category2 :	{
+			name : 'Black people', //Will appear in the data.
+			title : {
+				media : {word : 'Black people'}, //Name of the category presented in the task.
+				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
+				height : 4 //Used to position the "Or" in the combined block.
+			}, 
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+                {image: 'bm1_nc.jpg'},
+                {image: 'bm2_nc.jpg'},
+                {image: 'bm3_nc.jpg'},
+				{image: 'bf1_nc.jpg'},
+                {image: 'bf2_nc.jpg'},                 
+				{image: 'bf3_nc.jpg'}  
+			], 
+			//Stimulus css
+			stimulusCss : {color:'#31940F','font-size':'1.8em'}
+		},
+			attribute1 :
+			{
+				name : 'Good',
+				title : {
+					media : {word : 'Good'},
+					css : {color:'#0000FF','font-size':'1.8em'},
+					height : 4 //Used to position the "Or" in the combined block.
+				},
+				stimulusMedia : [ //Stimuli content as PIP's media objects
+					{word: 'Friendship'},
+					{word: 'Magnificent'},
+					{word: 'Pleasure'},
+					{word: 'Enjoy'},
+					{word: 'Terrific'},
+					{word: 'Appealing'}					
+				],
+				//Stimulus css
+				stimulusCss : {color:'#0000FF','font-size':'2.3em'}
+			},
+			attribute2 :
+			{
+				name : 'Bad',
+				title : {
+					media : {word : 'Bad'},
+					css : {color:'#0000FF','font-size':'1.8em'},
+					height : 4 //Used to position the "Or" in the combined block.
+				},
+				stimulusMedia : [ //Stimuli content as PIP's media objects
+					{word: 'Disaster'},
+					{word: 'Horrible'},
+					{word: 'Disgust'},
+					{word: 'Sick'},
+					{word: 'Despise'},
+					{word: 'Hurtful'}	
+				],
+				//Stimulus css
+				stimulusCss : {color:'#0000FF','font-size':'2.3em'}
+			},	
+            randomBlockOrder : true,			
+			base_url : {//Where are your images at?
+				image : 'https://tmo286.github.io/scripts-for-Qualtrics/images/'
+			} 			
+	});
+});
